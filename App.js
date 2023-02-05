8e4f78130321723a3e139cf227010c2db39b90c4
rijan999 <rijanmaharjan111@gmail.com>
2023-02-05 12:08:59 +0545

    new commit

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar} from 'react-native';
+import { Searchbar } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+ 
+      <SafeAreaView style={styles.container}>
+      <Searchbar placeholder="Search"/>
+        <View style={{padding:16,backgroundColor:'green'}}>
+     
+        </View>
+        <View style={{flex:1,padding:16, backgroundColor:'red'}}>
+        <Text>Body</Text>
+        <View style={{flex:1, alignItems: 'center', justifyContent:'center'}}>
+          <Text style={{fontSize:50,color:'white' }}>Hello</Text>
+
+        </View>
+        </View>
+        <StatusBar style='auto' />
+     </SafeAreaView>
+
+  
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
 });
